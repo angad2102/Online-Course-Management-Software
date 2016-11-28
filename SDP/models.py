1a28698222537e37010392a6807eeb52fe992670
Angad S <angads@tactsky.com>
2016-11-28 11:16:20 +0800

    setting the basic structure of project and database

---
+++ SDP/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+# class Author(models.Model):
+# 	name = models.CharField(max_length=200)
+
+
+
+# class Article(models.Model):
+# 	author = models.ForeignKey(Author, on_delete=models.CASCADE)
+# 	title = models.CharField(max_length=200)
+# 	content = models.TextField()
+# 	tags = models.ManyToManyField(Tag)
+
+class Category(models.Model):
+	name = models.CharField(max_length=200)
+
+class Course(models.Model):
+	name = models.CharField(max_length=200)
+	course_detail = models.TextField()
+	OPEN = 'OP'
+	DRAFT = 'DR'
+	CLOSED = 'CL'
+	STATUS_CHOICES = (
+		(OPEN, 'Open'),
+		(DRAFT, 'Draft'),
+		(CLOSED, 'Closed'),
+	)
+	status = models.CharField(
+		max_length=2,
+		choices = STATUS_CHOICES,
+		default = DRAFT,
+	)
+	category = models.ForeignKey(Category) 
+	instructor =  models.ForeignKey(User) 
+
+class Module(models.Model):
+	name = models.CharField(max_length=200)
+	sequence = models.IntegerField()
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)   
+
+class Component(models.Model):
+	name = models.CharField(max_length=200)
+	typ = models.CharField(max_length=200)
+	content = models.TextField()
+	sequence = models.IntegerField()
+	module = models.ForeignKey(Module, on_delete=models.CASCADE)   
+
+class CourseParticipantMap(models.Model):
+	participant =  models.ForeignKey(User) 
+	course =  models.ForeignKey(Course)
+	progress = models.CharField(max_length=200)
